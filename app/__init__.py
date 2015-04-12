8d0fb8df69ad98ba758ade068cafd617fd3fa5d5
Aishi Jiang <jiangaishi@gmail.com>
2015-04-12 01:16:05 -0400

    added sql import in init

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 # Import flask and template operators
 from flask import Flask
 from flask_httpauth import HTTPBasicAuth
+from flask.ext.sqlalchemy import SQLAlchemy
 
 # Define the WSGI application object
 app = Flask(__name__)
