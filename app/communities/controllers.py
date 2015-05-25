908ec98aba56ccdfe9d4207128fcc1afda339e8d
Aishi Jiang <jiangaishi@gmail.com>
2015-05-25 17:01:18 -0400

    cors

--- app/communities/controllers.py
+++ app/communities/controllers.py
@@ -74,7 +74,7 @@ def get_specific_issues(issue_id):
     issuelist = Issue.query.filter_by(id=issue_id)
     return jsonify({"issue" : [issue.serialize() for issue in issuelist]})
 
-@communities.route('/issue/<int:issue_id>/plan/create', methods=['POST'])
+@communities.route('/issue/<int:issue_id>/pla/create', methods=['POST'])
 @auth.login_required
 def create_action_plan(issue_id):
     plan = request.json.get('plan')
