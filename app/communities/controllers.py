9355e3c9c125bb4477ca1fc320513b37cdc93c18
Aishi Jiang <jiangaishi@gmail.com>
2015-05-25 16:49:37 -0400

    cors

--- app/communities/controllers.py
+++ app/communities/controllers.py
@@ -51,24 +51,6 @@ def get_all_issues():
 @communities.route('/<int:community_id>/issue/create', methods=['POST'])
 @auth.login_required
 def create_issue(community_id):
-    title = request.json.get('title')
-    info = request.json.get('article')
-    author_id = g.user.id
-    picture = request.json.get('picture')
-
-    if title is None or info is None or picture is None:
-        abort(400) # missing arguments
-
-    issue = Issue(title, info, author_id, picture)
-    issue.community_id = community_id
-    db.session.add(issue)
-
-    event = Event(1, g.user.id)
-    event.issue_id = issue.id
-    db.session.add(event)
-
-    db.session.commit()
-    response = {'status':200}
     return jsonify(**response)
 
 #@communities.route('<int:community_id>/issue/update/<int:issue_id>', methods=['PUT, GET'])
